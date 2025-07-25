4e6ddc5c4486ab9c1adf546b6130f20b8d952ca1
Morph <50250507+MiForgeMC@users.noreply.github.com>
2025-07-25 21:20:21 +0200

    main 0.1
    внедрил печать

--- main.py
+++ main.py
@@ -2,32 +2,42 @@ from PIL import ImageGrab
 import pytesseract
 from random import uniform
 import time
+import pyautogui
 
-# (опционально) путь к tesseract.exe
+
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\HyperBeast_\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
-# Снимок части экрана (левая, верхняя, правая, нижняя координаты)
 bbox = (200, 400, 1150, 700)
-img = ImageGrab.grab(bbox=bbox)
 
-# Распознавание текста
+time.sleep(2) #Время на переход в нужное окно
+
 start = time.time()
 total = 0
 
 while True:
-    text_arr = pytesseract.image_to_string(img, lang='eng').replace("\n", ' ').split(' ')
+    img = ImageGrab.grab(bbox=bbox) #Считываю изображние
+    text_arr = pytesseract.image_to_string(img, lang='eng').replace("\n", ' ').split(' ') #Преобразую изображение в массив слов
+    print(text_arr)
     if time.time()-start <= 15:
         for text in text_arr:
             
             if text != "":
-                for t in text:
-                    end = time.time()
-                    if end-start >= 15:
-                        break
-                    time.sleep(uniform(0.09, 0.2))
-                    print(f"{t} - {end-start}")
-                    total+=1
-                print()
+                # for t in text:
+                #     end = time.time()
+                #     if end-start >= 15:
+                #         break
+                #     # time.sleep(uniform(0.09, 0.2))
+                #     # print(f"{t} - {end-start}")
+                    
+                # # print()
+
+                end = time.time()
+                if end-start >= 15:
+                    break
+                time.sleep(0.1)
+                pyautogui.write(text, interval=uniform(0.05, 0.15))
+                total+=1
+                pyautogui.press("space")
     else:
         break
 
